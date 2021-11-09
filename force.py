6b42e26c8122ef4f9302d576d275a6805796ab48
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2021-11-09 16:54:17 -0300

    Add files via upload

--- force.py
+++ force.py
@@ -74,15 +74,16 @@ def Main():
   elif passi == "2":
      os.system('clear')
      print(f"{BANNER}")
-     sctoken = input("COLE O PASS: ")
+     sctoken = input(f"COLE O PASS: {GREEN}")
   elif passi == "0":
-     exit()
+     Main()
   else:
      Main()
    
- 
+   
+  op = "VIVO"
   UrlReward="http://e.vivo.ddivulga.com/api/clickEvent"
-  data = f"slotId=%5B%7B%22slotId%22%3A%22101%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22102%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22103%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22104%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%5D&pageId=660224&insertionId=&insertionUUID=&insertionType=&referer=http%3A%2F%2Finternetgratis.vivo.com.br%2F&accessPass={sctoken}"
+  data = f"slotId=%5B%7B%22slotId%22%3A%22101%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22102%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22103%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22104%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22105%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22106%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22107%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22108%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%2C%7B%22slotId%22%3A%22109%22%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%5D&pageId=660224&insertionId=&insertionUUID=&insertionType=&referer=http%3A%2F%2Finternetgratis.vivo.com.br%2F&accessPass={sctoken}"
   url = "http://e.vivo.ddivulga.com/api/v2.1/fetch"
   headers = CaseInsensitiveDict()
   headers["Origin"] = "http://a.vivo.ddivulga.com"
@@ -125,12 +126,12 @@ def Main():
   elif passi == "2":
      os.system('clear')
      print(f"{BANNER}")
-     sctoken = input("COLE O PASS: ")
+     sctoken = input(f"COLE O PASS: {GREEN}")
   elif passi == "0":
-     exit()
+     Main()
   else:
      Main()
-     
+  op = "OI"
   UrlReward="http://e.oi.ddivulga.com/api/clickEvent"
   data = f"slotId=%5B%7B%22slotId%22%3A301%2C%22target%22%3A%22os%3Dandroid%3BtailCity%3Dmanaus%3BtailtState%3Damazonas%3BtailCountry%3Dbr%22%7D%5D&pageId=822068&insertionId=&insertionUUID=&insertionType=ANY&referer=&accessPass={sctoken}"
   url = "http://e.oi.ddivulga.com/api/v2.1/fetch"
@@ -142,21 +143,27 @@ def Main():
    
 
  elif option == "0":
+     os.system('clear')
      exit()
  else:
    os.system('clear')
    Main()
-
- buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward)
+      
+ advid = [ ]
+ impressionid = [ ]
+ tokens_on = 0
+ iu = 0
+ AdvName = [ ]
+ 
+ buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, tokens_on, AdvName, iu, op)
 
  
-def buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward):
+def buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, tokens_on, AdvName, iu, op):
  os.system('clear')
- print(f"{BANNER}\nPROCURANDO ARQUIVOS QUE ROTIA...")
+ print(f"{NADA}{BANNER}\nPROCURANDO ARQUIVOS QUE ROTIA...")
  time.sleep(4)
  try:
-  fetch = requests.post(url, headers=headers, data=data).text
-  X9deToken = json.loads(fetch)
+  X9deToken = json.loads(requests.post(url, headers=headers, data=data).text)
 
  except:
   os.system('clear')
@@ -164,75 +171,85 @@ def buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlRew
   time.sleep(5)
   Main()
 
- advid = [ ]
- impressionid = [ ]
- tokens_on = 0
- iu = 0
- AdvName = [ ]
- AdvReward = [ ]
- AdvMensagem = [ ]
-
- while iu < 18:
+ tokensnovos = 0
+ while iu < 4:
   iu = iu + 1
- 
   try:
-   advid.append(X9deToken[f'10{iu}']['advId'])
-   
-   
   
-   impressionid.append(X9deToken[f'10{iu}']['impressionEventId'])
+   advidchecar = (X9deToken[f'10{iu}']['advId'])
    
-   AdvName.append(X9deToken[f'10{iu}']['advName'])
+   if advidchecar in advid:
+    pass
+   else:
+    advid.append(X9deToken[f'10{iu}']['advId'])
    
-   AdvReward.append(X9deToken[f'10{iu}']['rewardWebMessage2'])
+   impressionidcheck = (X9deToken[f'10{iu}']['impressionEventId'])
+   
+   if impressionidcheck in impressionid:
+    pass
+   else:
+    impressionid.append(X9deToken[f'10{iu}']['impressionEventId'])
+   
+   AdvNamecheck = (X9deToken[f'10{iu}']['advName'])
+   
+   if AdvNamecheck in AdvName:
+    pass
+   
+   else:   
+    AdvName.append(X9deToken[f'10{iu}']['advName'])
    
    tokens_on = tokens_on + 1
-  
+   tokensnovos = tokensnovos + 1
   except:
    pass
    
  Numero = X9deToken['msisdn']
- if tokens_on == '0':
-   os.system('clear')
-   print(f"{BANNER}")
-   print(f"{GREEN}{tokens_on} TOKENS FORAM ENCONTRADOS{NADA}")
-   print()
-   print(f"{GREEN}NUMERO DE TELEFONE:{NADA} {GREEN}{Numero}{NADA}")
-   print()
-   print(f"0 - VOLTAR {RED}NENHUM TOKEN ENCONTRADO{NADA}")
-   print()
-   resl = input("Escolha uma opção: ")
-   Main()
- else:
-   os.system('clear')
-   print(f"{BANNER}")
-   print(f"{GREEN}{tokens_on} TOKENS FORAM ENCONTRADOS{NADA}")
-   print()
-   print(f"{GREEN}NUMERO DE TELEFONE:{NADA} {GREEN}{Numero}{NADA}")
-   print()
-   print(f"1 - TESTAR TOKENS ENCONTRADOS {GREEN}INICIAR O ENVIO DOS MBS{NADA}")
-   print(f"0 - VOLTAR {RED}MERDA DE CHIP{NADA}")
-   print()
-   resl = input("Escolha uma opção: ")
-   
-   if resl == '1':
-     print()
-     
-   else:
-     Main()
+ FreeFire(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, AdvName, iu, op, tokensnovos, tokens_on, Numero)
 
- headers = CaseInsensitiveDict()
- headers["Content-Type"] = "application/x-www-form-urlencoded"
+def FreeFire(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, AdvName, iu, op, tokensnovos, tokens_on, Numero):
+
+
+ os.system('clear')
+ print(f"{BANNER}")
+ print(f"{GREEN}{tokensnovos} TOKENS NOVOS FORAM ENCONTRADOS{NADA}")
+ print()
+ print(f"{GREEN}TOTAL DE TOKENS: {NADA} {GREEN}{tokens_on}{NADA}")
+ print(f"{GREEN}NUMERO DE TELEFONE:{NADA} {GREEN}{Numero}{NADA}")
+ print()
+ print(f"1 - BUSCAR MAIS TOKENS {GREEN}ESTOQUE DE MBS{NADA}")
+ print(f"2 - TESTAR TOKENS ENCONTRADOS {GREEN}FF PING BAIXO ON{NADA}")
+ print(f"3 - TESTAGEM INDIVIDUAL {GREEN}SOCAR NA {op} AOS POUCOS{NADA}")
+ print(f"0 - VOLTAR {RED}MERDA DE CHIP{NADA}")
+ print()
+ resl = input("Escolha uma opção: ")
+ 
+ if resl == '2':
+  pass
+   
+ elif resl == '1':
+  buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, tokens_on, AdvName, iu, op)    
+  
+ elif resl == '3':
+   TestagemIndividual(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data, tokensnovos, tokens_on)
+ 
+ elif resl == '0':
+  Main()
+ else:
+  FreeFire(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, AdvName, iu, op, tokensnovos, tokens_on, Numero)
+ 
+ print()
+ headersi = CaseInsensitiveDict()
+ headersi["Content-Type"] = "application/x-www-form-urlencoded"
 
  testador = 0
  testestokens = 0
 
 
- Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, AdvReward, UrlReward)
+ Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data)
 
 
-def Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, AdvReward, UrlReward):
-
+def Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data):
+ os.system('termux-wake-lock')
  for token in range(tokens_on):
   
   testador = testador + 1
@@ -252,32 +269,123 @@ def Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tok
       resp = requests.post(UrlReward, headers=headers, data=f"advId={advid[token]}&eventImpressionId={impressionid[token]}&apass={sctoken}").text
       
       if 'evtClickId' in resp:
-        print(f"{GREEN}Ganhei 10 MBs - {contagem}{NADA}")
+        print(f"{GREEN}PARTIDAS NO FREE FIRE - {contagem}{NADA}")
       
       else:
-        print(f"{RED}Merda de ads{NADA}")
+        print(f"{RED}MERDA DE ADS{NADA}")
         time.sleep(4)
         contagem = reapit
   except:
     pass
+    
+ testestokens = testestokens + 1
+ Acabou(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data)
+
+def Acabou(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data):
 
  os.system('clear')
- testestokens = testestokens + 1
  print(f"{BANNER}")
  print(f"{GREEN}NUMERO DE TELEFONE: {GREEN}{Numero}{NADA}")
- print(f"TESTE COM ESSES TOKENS: {GREEN}{testestokens}{NADA}")
+ print(f"{GREEN}TESTE COM ESSES TOKENS: {testestokens}{NADA}")
  print()
- print(f"1 - TESTAR TOKENS NOVAMENTE {GREEN}ASSIM A VIVO VAI PRO SACO{NADA}")
- print(f"0 - Voltar {RED}MERDA DE CHIP{NADA}")
+ print(f"1 - BUSCAR NOVOS TOKENS {GREEN}ESTOQUE DE MBS{NADA}")
+ print(f"2 - TESTAR TOKENS NOVAMENTE {GREEN}ASSIM A {op} VAI PRO SACO{NADA}")
+ print(f"0 - VOLTAR {RED}MERDA DE CHIP{NADA}")
+ print()
+ option = input("Escolha uma Opção: ")
+ 
+ if option == '2':
+    testador = 0
+    Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data)
+ 
+ if option == '1':
+  advid = [ ]
+  impressionid = [ ]
+  tokens_on = 0
+  iu = 0
+  AdvName = [ ]
+  buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, tokens_on, AdvName, iu, op)
+ elif option == "0":
+    testador = 0
+    Main()    
+    
+ else:
+    Acabou(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data)
+    
+
+def TestagemIndividual(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data, tokensnovos, tokens_on):
+ os.system('clear')
+ print(f"{BANNER}")
+ print(f"{GREEN}NUMERO DE TELEFONE: {GREEN}{Numero}{NADA}")
+ print()
+ try:
+  contar = 0
+  for i in range(tokens_on):
+   contar = contar + 1
+   print(f"ADS {contar}")
+   print(f"{YELLOW}Adv Name: {AdvName[i]}")
+   print(f"ImpressionID: {impressionid[i]}")
+   print(f"AdvID: {advid[i]}{NADA}")
+   print()
+ except:
+  buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, tokens_on, AdvName, iu, op)
+ 
+ print("0 - VOLTAR") 
+ print()
+ try:
+  Escolha = int(input("Escolha um ADS: "))
+  if Escolha == 0:
+   FreeFire(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, AdvName, iu, op, tokensnovos, tokens_on, Numero)
+
+  else:
+   pass
+ except:
+  TestagemIndividual(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data, tokensnovos, tokens_on)
+  
+ print()
+ print(f"{NADA}{LETRAPRETA}           TESTAGEM INICIADA           {NADA}")
+ try:
+  contagem = 0
+  Escolha = Escolha - 1
+  while contagem < reapit:
+      
+   time.sleep(0.7)
+   contagem = contagem + 1
+      
+   resp = requests.post(UrlReward, headers=headers, data=f"advId={advid[Escolha]}&eventImpressionId={impressionid[Escolha]}&apass={sctoken}").text
+      
+   if 'evtClickId' in resp:
+    print(f"{GREEN}PARTIDAS NO FREE FIRE - {contagem}{NADA}")
+      
+   else:
+    print(f"{RED}MERDA DE ADS{NADA}")
+    time.sleep(4)
+    contagem = reapit
+ except:
+   pass  
+  
+ os.system('clear')
+ print(f"{BANNER}")
+ print(f"{GREEN}NUMERO DE TELEFONE: {GREEN}{Numero}{NADA}")
+ print()
+ print(f"1 - ESCOLHER OUTRO TOKEN {GREEN}SOCAR MAIS UM POUCO{NADA}")
+ print(f"2 - BUSCAR NOVOS TOKENS {GREEN}ASSIM A {op} VAI PRO SACO{NADA}")
+ print(f"0 - VOLTAR {RED}MERDA DE CHIP{NADA}")
  print()
  option = input("Escolha uma Opção: ")
  
  if option == '1':
-    testador = 0
-    Envio(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, tokens_on, testador, testestokens, sctoken, reapit, headers, AdvName, AdvReward, UrlReward)
+  TestagemIndividual(BANNER, NADA, LETRAPRETA, advid, impressionid, GREEN, RED, Numero, sctoken, reapit, headers, AdvName, UrlReward, url, op, iu, data, tokensnovos, tokens_on)
  
- else:
-    testador = 0
-    Main()
-
+ elif option == '2':
+  buscar(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, tokens_on, AdvName, iu, op)
+ 
+ elif option == '3':
+  Main()
+  
+ else: 
+  FreeFire(BANNER, url, headers, data, RED, GREEN, NADA, sctoken, reapit, UrlReward, impressionid, advid, AdvName, iu, op, tokensnovos, tokens_on, Numero)
+  
+  
+    
 Main()
